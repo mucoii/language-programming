4ef464c916e97015cb4f16e17e8216a144d4c54a
Mu Coi <76393322+mucoii@users.noreply.github.com>
2023-10-23 14:20:57 +0700

    ex2_gioi_thieu_cpp
    Introduction about C plus plus

--- cPlusPlus/source_code/ex2_gioi_thieu_cpp/hello-world.cpp
+++ cPlusPlus/source_code/ex2_gioi_thieu_cpp/hello-world.cpp
@@ -1,1 +1,17 @@
+/*
+Ex2:  Xuat ra man hinh chu Hello World!
+*/
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main() 
+{
+ cout << "Hello World!";
+ getch();
+ return 0;
+}
 
