1a3cb8926918473716f274358243e987dff035ec
Mu Coi <76393322+mucoii@users.noreply.github.com>
2023-10-24 14:30:16 +0700

    Create ex4_kieu_du_lieu.cpp

---
+++ cPlusPlus/source_code/ex4_kieu_du_lieu/ex4_kieu_du_lieu.cpp
@@ -0,0 +1,21 @@
+ /*
+    Ex_4 Kieu du lieu va dinh danh 
+ */
+ 
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+ 
+int main() {
+    
+    cout << "Size of char: " << sizeof(char) << endl; 
+    cout << "Size of int: " << sizeof(int) << endl;
+    cout << "Size of float: " << sizeof(float) << endl;
+    cout << "Size of double: " << sizeof(double) << endl;
+    cout << "Size of long in: " << sizeof(long int) << endl;
+    
+  getch(); 
+    return 0;   
+} 
